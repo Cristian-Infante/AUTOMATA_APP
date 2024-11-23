9c3bab1a929c09aef1c5cf99a299274d91387a20
Cristian-Infante <cinfantecanelo@gmail.com>
2024-11-23 14:56:55 -0500

    Initial commit

--- AUTOMATA_APP/settings.py
+++ AUTOMATA_APP/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from django.contrib import staticfiles
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zy*qz9m8ggoda&z1(ws*va^tz5_o3_+eq8jhros_1ocq&h&605'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.railway.app']
 
 
 # Application definition
@@ -118,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
---
+++ Dockerfile
@@ -0,0 +1,20 @@
+﻿# Usa una imagen base oficial de Python
+FROM python:3.10-slim
+
+# Establece el directorio de trabajo
+WORKDIR /app
+
+# Copia los archivos del proyecto
+COPY . .
+
+# Instala las dependencias
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Ejecuta collectstatic (sin interacción)
+RUN python manage.py collectstatic --noinput
+
+# Expone el puerto por defecto de Django
+EXPOSE 8000
+
+# Comando para ejecutar el servidor
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
